4d599b2eb3a2aea06e561c63a623d61a5760728d
stanislavBozhanov <stanislav.bozhanov@gmail.com>
2014-06-16 12:16:25 +0300

    Fixed filter

--- 022.LectureThreeHomework/Expenditures/index.php
+++ 022.LectureThreeHomework/Expenditures/index.php
@@ -6,7 +6,6 @@ $pageTitle = 'Списък с разходи';
 <a href="form.php">Добави разход</a>
 <form method="post">
     <select name="filter">
-        <option value=""></option>
         <option value="0">Всички</option>
         <?php
         foreach ($product_types as $key=>$type) {
@@ -19,44 +18,39 @@ $pageTitle = 'Списък с разходи';
 <?php
 if(file_exists('data.txt')){
     $file_information = file('data.txt');
-    $filter = 0;
-    if($_POST){
-        $filter = (int)$_POST['filter'];
-        $total = 0;
+    $total = 0;
+    echo
+    '<table border="1">
+        <tr>
+        <td>' . 'Дата' . '</td>
+        <td>' . 'Разход' . '</td>
+        <td>' . 'Сума' . '</td>
+        <td>' . 'Вид' . '</td>
+        </tr>';
 
-        echo
-        '<table border="1">
-            <tr>
-            <td>' . 'Дата' . '</td>
-            <td>' . 'Разход' . '</td>
-            <td>' . 'Сума' . '</td>
-            <td>' . 'Вид' . '</td>
-            </tr>';
-        foreach ($file_information as $line) {
-            $columns = explode('!', $line);
-            if($filter && $filter != $columns[3]) {
-                continue;
-            }
-            $total += (float)$columns[2];
-            echo '<tr>
-                <td>' . $columns[0] . '</td>
-                <td>' . $columns[1] . '</td>
-                <td>' . $columns[2] . '</td>
-                <td>' . $product_types[trim($columns[3])] . '</td>
-                <td><a href="index.php">X</a></td>
-                </tr>';
+    foreach ($file_information as $line) {
+        $columns = explode('!', $line);
+        if(isset($_POST['filter']) && $_POST['filter'] > 0 && (int)$_POST['filter']!=(int)$columns[3]) {
+            continue;
         }
-        echo    '<tr>
-            <td>' . '----' . '</td>
-            <td>' . '----' . '</td>
-            <td>' . sprintf("%.2f",$total) . '</td>
-            <td>' . '----' . '</td>
-            </tr>
-            </table>';
+        $total += (float)$columns[2];
+        echo '<tr>
+            <td>' . $columns[0] . '</td>
+            <td>' . $columns[1] . '</td>
+            <td>' . number_format($columns[2], 2, '.', '') . '</td>
+            <td>' . $product_types[trim($columns[3])] . '</td>
+            <td><a href="index.php">X</a></td>
+            </tr>';
     }
-    else {
-        echo 'Изберете вид разходи';
-    }
+
+    echo    '<tr>
+        <td>' . '----' . '</td>
+        <td>' . '----' . '</td>
+        <td>' . number_format($total, 2, '.', '') . '</td>
+        <td>' . '----' . '</td>
+        </tr>
+        </table>';
 }
+
 include 'includes/footer.php';
 ?>
